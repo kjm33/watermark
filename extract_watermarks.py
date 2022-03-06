ac3568b6a7efae606a2c45fb38e4d4c3c38c6dea
kamilm <>
2022-03-06 14:52:07 +0100

    Automated thresholding (Otsu) for increasing watermarks visibility.

--- extract_watermarks.py
+++ extract_watermarks.py
@@ -9,10 +9,28 @@ laplacian = cv2.Laplacian(avg_img_gray, cv2.CV_64F)
 laplacian_normalized = cv2.convertScaleAbs(laplacian)
 
 cv2.imshow('normalized Lap', laplacian_normalized)
-cv2.waitKey(0)
+
 
 laplacian_normalized_out_path = Path("out") / "laplacian.jpg"
 
 if not laplacian_normalized_out_path.exists():
     cv2.imwrite(str(laplacian_normalized_out_path), laplacian_normalized)
+
+# watermarks are now barely visible (gray patterns on black background) so let's increase contrast with thresholding for
+# further processing
+
+# on the first sight foreground/background should be easy to separate with Otsu
+
+otsu = cv2.threshold(laplacian_normalized, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+cv2.imshow("Otsu", otsu)
+# bingo!
+
+lap_bin_path = Path("out") / "laplacian_bin_Otsu.jpg"
+
+if not lap_bin_path.exists():
+    cv2.imwrite(str(lap_bin_path), otsu)
+
+
+cv2.waitKey(0)
 
