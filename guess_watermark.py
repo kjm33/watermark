5b78d1942148c14dd6be54321c6a7ca03a414e3e
kamilm <>
2022-03-10 21:24:35 +0100

    Take GD params from args.

--- guess_watermark.py
+++ guess_watermark.py
@@ -1,13 +1,24 @@
 from pathlib import Path
+import argparse
 import numpy as np
 import cv2
+
+
+def get_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--iterations", type=int, default=800,
+                        help="The number of iterations of GD")
+    parser.add_argument("-a", "--alpha", type=float, default=0.01,
+                        help="GD learning rate")
+    return parser.parse_args()
 
 
 def mse(img1, img2):
     return np.square(np.subtract(img1, img2)).mean()
 
 
-ITERATIONS = 800
+args = get_params()
+iterations = args.iterations
 
 dst = cv2.imread("./samples/center_cross_25_x_25.png", 0)
 dst_mode = 244
@@ -18,13 +29,13 @@ np.random.seed(42)
 watermark_weights = np.random.rand(*dst.shape)
 gamma = 0.0
 
-alpha = 0.01
+alpha = args.alpha
 # TODO: test different values (simulated annealing?)
 
 trans_ratio = 0.8  # alpha is already used as a gradient descent learning rate
 trans_ratio_neg = 1.0 - trans_ratio  # beta
 
-for _ in range(ITERATIONS):
+for _ in range(iterations):
     watermark = watermark_weights * 255  # matrix of floats
     watermark = watermark.astype(np.uint8)  # back to uchar
     # ^^ small changes in weights can be ignored/lost here due to casting (rounding?)
@@ -39,13 +50,10 @@ for _ in range(ITERATIONS):
     """
     blended = cv2.addWeighted(src1=background, alpha=trans_ratio, src2=watermark, beta=trans_ratio_neg, gamma=gamma)
 
-
     print(mse(dst, blended))
 
     delta = dst - blended
-
     delta_weights = delta / 255
-
     watermark_weights -= delta_weights * alpha
 
-cv2.imwrite(str(Path("out") / f"guessed_wm_{ITERATIONS}_iters.jpg"), watermark)
+cv2.imwrite(str(Path("out") / f"guessed_wm_{iterations}_iters.jpg"), watermark)
