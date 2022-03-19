9528168254b1dda3eddf3fe4083215271675ab36
kamilm <>
2022-03-19 14:01:01 +0100

    Use right pattern file, remove bluring and drawing contours.

--- detect_binary_watermarks.py
+++ detect_binary_watermarks.py
@@ -1,39 +1,21 @@
 from pathlib import Path
 import numpy as np
 import cv2
-import random as rng
 
-# TODO: median filter -> contour detection, common shape and positions of centers
-
-laplacian_normalized_out_path = Path("out") / "laplacian.jpg"
+laplacian_normalized_out_path = Path("out") / "laplacian_bin_Otsu.jpg"
 watermarks_gray = cv2.imread(str(laplacian_normalized_out_path), 0)
-watermarks_median = cv2.medianBlur(watermarks_gray, 5)
-thresh = 3
-watermarks_bin = cv2.threshold(watermarks_median, thresh, 255, cv2.THRESH_BINARY)[1]
-
-cv2.imshow("median", watermarks_bin)
 
 
-contours, hierarchy = cv2.findContours(watermarks_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+contours, hierarchy = cv2.findContours(watermarks_gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 cnt_sorted = sorted(contours, key=cv2.contourArea, reverse=True)
 true_watermarks = cnt_sorted[:15]
 
-# y = 20, 320, 620 -> 3 rows
-# x = 17, 317, 617, 917, 1217 -> 5 columns
+# y = 18, 318, 618 -> 3 rows
+# x = 16, 316, 616, 916, 1216 -> 5 columns
+# w = 264
+# h = 264 :)
 
-for idx, cnt in enumerate(cnt_sorted, start=1):
+for idx, cnt in enumerate(true_watermarks, start=1):
     x, y, w, h = cv2.boundingRect(cnt)
-    area = cv2.contourArea(cnt)
     print(f"{idx} {x} {y} {w} {h}")
 print()
-
-# from https://docs.opencv.org/4.x/df/d0d/tutorial_find_contours.html
-rng.seed(42)
-drawing = np.zeros((watermarks_bin.shape[0], watermarks_bin.shape[1], 3), dtype=np.uint8)
-for i in range(len(contours)):
-    color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-    cv2.drawContours(drawing, contours, i, color, 2, cv2.LINE_8, hierarchy, 0)
-
-cv2.imshow('Contours', drawing)
-
-cv2.waitKey(0)
