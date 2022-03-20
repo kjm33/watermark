7789f71f96a6fd7ab76a5cb47cc29374517108da
kamilm <>
2022-03-20 13:45:58 +0100

    Select 10 top watermarks with the most uniform background.

--- split_img_into_watermarks.py
+++ split_img_into_watermarks.py
@@ -6,20 +6,34 @@ COLS = 16, 316, 616, 916, 1216
 ROWS = 18, 318, 618
 HEIGHT = WEIGHT = 264
 
-test_img_path = Path('~/Pictures/sesja_natka/sesja_Natka/DSC_8779.jpg').expanduser()
+
+def split_image(img_path: Path):
+    img = cv2.imread(str(img_path))
+
+    if img.shape != (1080, 1619, 3):
+        return
+
+    img_idx = 0
+    for row in ROWS:
+        for col in COLS:
+            roi = img[row:row+HEIGHT, col:col+WEIGHT]
+            img_idx += 1
+            roi_path = f"{img_path.name.replace('.jpg', '')}_{img_idx}.png"
+            roi_full_path = out_dir / roi_path
+            cv2.imwrite(str(roi_full_path), roi)
+
+            roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            std = np.std(roi_gray)
+            # get top 10 chunks by
+            # awk NF std_of_watermarks.txt | sort -g -k2,2 | head -n 10
+            print(f"{roi_path} {std:.2f}")
+
+
 out_dir = Path("watermarks")
+input_dir = Path('~/Pictures/sesja_natka/sesja_Natka/').expanduser()
 
-test_img = cv2.imread(str(test_img_path))
+img_paths = input_dir.glob("*.jpg")
 
-img_idx = 0
-for row in ROWS:
-    for col in COLS:
-        roi = test_img[row:row+HEIGHT, col:col+WEIGHT]
-        img_idx += 1
-        roi_path = f"{test_img_path.name.replace('.jpg', '')}_{img_idx}.png"
-        roi_full_path = out_dir / roi_path
-        cv2.imwrite(str(roi_full_path), roi)
-
-        roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        std = np.std(roi_gray)
-        print(f"{roi_path} {std:.2f}")
+for img_path in sorted(img_paths):
+    split_image(img_path)
+    print()
