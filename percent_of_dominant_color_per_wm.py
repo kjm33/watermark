7296fc9e0212ba7b65c2cdfe294dc2478b493c1e
kamilm <>
2022-03-21 17:19:56 +0100

    Show percentage of a dominant pixel in a COLOR watermark.

--- percent_of_dominant_color_per_wm.py
+++ percent_of_dominant_color_per_wm.py
@@ -4,13 +4,14 @@ import numpy as np
 from collections import Counter
 
 input_dir = Path("./watermarks")
-wm_paths = input_dir.glob("*.png")
+wm_paths = input_dir.glob("*png")
 
 
 for wm_path in wm_paths:
-    wm_gray = cv2.imread(str(wm_path), cv2.IMREAD_GRAYSCALE)
-    unique, counts = np.unique(wm_gray.flatten(), return_counts=True)
-    freq = np.argmax(counts)
-    value = unique[freq]
-    percent = freq / (wm_gray.shape[0]*wm_gray.shape[1])*100
+    wm = cv2.imread(str(wm_path))
+    unique, counts = np.unique(wm.reshape(-1, 3), axis=0, return_counts=True)
+    idx = np.argmax(counts)
+    freq = counts[idx]
+    value = unique[idx]
+    percent = freq / (wm.shape[0]*wm.shape[1])*100
     print(f"{wm_path} {percent:.2f} {value}")
