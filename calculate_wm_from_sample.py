eb3f8cc18a66e2157eb31232f0f1714061b3dfd2
kamilm <>
2022-03-25 14:30:08 +0100

    Get gamma and normalize obtained watermark.

--- calculate_wm_from_sample.py
+++ calculate_wm_from_sample.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 sample_path = Path("./watermarks/DSC_8767_5.png")
 sample = cv2.imread(str(sample_path))
@@ -11,12 +12,17 @@ alpha = 0.85
 beta = 0.15
 background = np.full(sample.shape, MODE_COLOR[0], dtype=float)
 
-for alpha100 in range(85, 5, -5):
-    alpha = alpha100/100.0
-    beta = 1 - alpha
+bckg_ratio = alpha/beta
+watermark = sample.astype(float)/beta - bckg_ratio*background
+watermark_single_channel = watermark[:, :, 0]
 
-    bckg_ratio = alpha/beta
-    watermark = sample.astype(float)/beta - bckg_ratio*background
-    watermark_single_channel = watermark[:, :, 0]
-    print(f"{alpha} max {watermark_single_channel.max()}")
+max_pixel = watermark_single_channel.max()
+gamma = int(math.ceil(max_pixel) - 255)  # 63
 
+watermark_normalized = cv2.convertScaleAbs(watermark)
+
+cv2.imshow("watermark scaled", watermark_normalized)
+# cv2.imwrite("watermark_calculated.png", watermark_normalized)
+cv2.waitKey()
+
+# TODO: addWeighted(background, calculated_wm, w/ params) <- compare by ssim with sample image
