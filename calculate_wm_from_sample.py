431bf932d97308b90020a5cef020c367d13b1233
kamilm <>
2022-03-26 22:13:52 +0100

    Remove watermark from a test chunk.

--- calculate_wm_from_sample.py
+++ calculate_wm_from_sample.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 
 sample_path = Path("./watermarks/DSC_8767_5.png")
 sample = cv2.imread(str(sample_path))
@@ -21,10 +19,11 @@ watermark_normalized = cv2.convertScaleAbs(watermark)
 
 reblended = cv2.addWeighted(src1=background.astype(np.uint8), alpha=alpha, src2=watermark_normalized, beta=beta, gamma=0)
 
-diff = sample - reblended
-diff_single_channel = diff[:, :, 0]
 
-plt.imshow(diff_single_channel)
-plt.colorbar()
-plt.title("Difference between original and restored images")
-plt.show()
+# test = alpha*src + beta*wm -> src = test/alpha - beta/alpha*wm
+test = cv2.imread("./watermarks/DSC_8511_8.png")
+test_restored = test/alpha - (beta/alpha)*watermark_normalized
+test_normalized = cv2.convertScaleAbs(test_restored)
+
+cv2.imshow("Test restored&normalized", test_normalized)
+cv2.waitKey()
