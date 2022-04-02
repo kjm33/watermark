515621f41d05d9f3eb790f57a321d2c82f66739d
kamilm <>
2022-04-02 14:18:21 +0200

    Test results with new alpha value.

--- deblend_directly/calculate_wm_from_sample.py
+++ deblend_directly/calculate_wm_from_sample.py
@@ -2,36 +2,48 @@ from pathlib import Path
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from common.watermark import get_alpha
 
 sample_path = Path("../watermarks/DSC_8767_5.png")
 sample = cv2.imread(str(sample_path))
 MODE_COLOR = (244,)*3
 
-alpha = 0.85
-beta = 0.15
+alpha = 0.777997737556562  # calculated by linear regression
+beta = 1 - alpha
+
+
 background = np.full(sample.shape, MODE_COLOR[0], dtype=float)
 
 bckg_ratio = alpha/beta
 watermark = sample.astype(float)/beta - bckg_ratio*background
 watermark_single_channel = watermark[:, :, 0]
-np.savetxt("watermark_float.csv", watermark_single_channel, fmt='%.3f')
-
-
-watermark_normalized = cv2.convertScaleAbs(watermark)
-
-reblended = cv2.addWeighted(src1=background.astype(np.uint8), alpha=alpha, src2=watermark_normalized, beta=beta, gamma=0)
 
 
 # test = alpha*src + beta*wm -> src = test/alpha - beta/alpha*wm
-test = cv2.imread("../watermarks/DSC_8511_8.png")
-test_restored = test/alpha - (beta/alpha)*watermark_normalized
+# test = cv2.imread("../watermarks/DSC_8511_8.png")
+# test = cv2.imread("../watermarks/DSC_8485_1.png")
+test = cv2.imread("../watermarks/DSC_8500_10.png")
+test_restored = test/alpha - (beta/alpha)*watermark
 test_normalized = cv2.convertScaleAbs(test_restored)
 
-diff = test - test_restored
-diff_single_channel = diff[:, :, 0]
 
-plt.imshow(diff_single_channel)
-plt.colorbar()
-plt.title("Difference between original and restored images")
+mask = get_alpha()
+
+merged = test.copy()
+merged[np.where(mask == 255)] = test_normalized[np.where(mask == 255)]
+
+
+fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
+
+axs[0].set_title("original")
+axs[0].imshow(cv2.cvtColor(test, cv2.COLOR_BGR2RGB))
+
+axs[1].set_title('extracted WM')
+axs[1].imshow(watermark_single_channel)
+
+axs[2].set_title('restored')
+axs[2].imshow(cv2.cvtColor(merged.astype(np.uint8), cv2.COLOR_BGR2RGB))
+
 plt.show()
+
 
