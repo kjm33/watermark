4b99a21c9c5e818e4792373f9ca34225ed2db8d1
kamilm <>
2022-04-02 22:03:13 +0200

    Choosing a good example to test.

--- deblend_directly/calculate_wm_from_sample.py
+++ deblend_directly/calculate_wm_from_sample.py
@@ -20,9 +20,11 @@ watermark_single_channel = watermark[:, :, 0]
 
 
 # test = alpha*src + beta*wm -> src = test/alpha - beta/alpha*wm
-# test = cv2.imread("../watermarks/DSC_8511_8.png")
-# test = cv2.imread("../watermarks/DSC_8485_1.png")
-test = cv2.imread("../watermarks/DSC_8500_10.png")
+# test_bgr = cv2.imread("../watermarks/DSC_8511_8.png")
+test_bgr = cv2.imread("../watermarks/DSC_8724_5.png")
+# test_bgr = cv2.imread("../watermarks/DSC_8500_10.png")
+test = cv2.cvtColor(test_bgr, cv2.COLOR_BGR2RGB)
+watermark[np.where(watermark == MODE_COLOR)] = test[np.where(watermark == MODE_COLOR)]
 test_restored = test/alpha - (beta/alpha)*watermark
 test_normalized = cv2.convertScaleAbs(test_restored)
 
@@ -36,13 +38,14 @@ merged[np.where(mask == 255)] = test_normalized[np.where(mask == 255)]
 fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
 
 axs[0].set_title("original")
-axs[0].imshow(cv2.cvtColor(test, cv2.COLOR_BGR2RGB))
+axs[0].imshow(test)
 
 axs[1].set_title('extracted WM')
 axs[1].imshow(watermark_single_channel)
 
 axs[2].set_title('restored')
-axs[2].imshow(cv2.cvtColor(merged.astype(np.uint8), cv2.COLOR_BGR2RGB))
+# axs[2].imshow(cv2.cvtColor(merged.astype(np.uint8), cv2.COLOR_BGR2RGB))
+axs[2].imshow(test_normalized)
 
 plt.show()
 
