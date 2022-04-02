2651d7186669793d009a852c65201c985548c6bf
kamilm <>
2022-04-02 22:29:57 +0200

    Use common mask.

--- deblend_directly/calculate_wm_from_sample.py
+++ deblend_directly/calculate_wm_from_sample.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from common.watermark import get_alpha
+from common.watermark import get_common_mask
 
 sample_path = Path("../watermarks/DSC_8767_5.png")
 sample = cv2.imread(str(sample_path))
@@ -24,18 +24,18 @@ watermark_single_channel = watermark[:, :, 0]
 test_bgr = cv2.imread("../watermarks/DSC_8724_5.png")
 # test_bgr = cv2.imread("../watermarks/DSC_8500_10.png")
 test = cv2.cvtColor(test_bgr, cv2.COLOR_BGR2RGB)
-watermark[np.where(watermark == MODE_COLOR)] = test[np.where(watermark == MODE_COLOR)]
+
+mask = get_common_mask()
+watermark[np.where(mask == (0, 0, 0))] = test[np.where(mask == (0, 0, 0))]
 test_restored = test/alpha - (beta/alpha)*watermark
 test_normalized = cv2.convertScaleAbs(test_restored)
 
-
-mask = get_alpha()
 
 merged = test.copy()
 merged[np.where(mask == 255)] = test_normalized[np.where(mask == 255)]
 
 
-fig, axs = plt.subplots(1, 3, sharex=True, sharey=True)
+fig, axs = plt.subplots(1, 4, sharex=True, sharey=True)
 
 axs[0].set_title("original")
 axs[0].imshow(test)
@@ -46,6 +46,9 @@ axs[1].imshow(watermark_single_channel)
 axs[2].set_title('restored')
 # axs[2].imshow(cv2.cvtColor(merged.astype(np.uint8), cv2.COLOR_BGR2RGB))
 axs[2].imshow(test_normalized)
+
+axs[3].set_title('restored rounded')
+axs[3].imshow(np.around(test_normalized))
 
 plt.show()
 
